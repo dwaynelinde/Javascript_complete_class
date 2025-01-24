d3b5755715365b4a81430662e5fb103f0d0b598c
Dwayne Linde <lindedwayne@gmail.com>
2025-01-24 14:04:15 -0800

    I completed homework 1.

--- section2/script.js
+++ section2/script.js
@@ -1,4 +1,18 @@
 
 let js = "amazing";
 
-console.log(1 = 1 = 2); 
+console.log(1 + 1);
+console.log("hfjkahka");
+
+
+let NAMEtyo = "NAMEtyowqoytoq";
+
+// homework 1 
+
+let country = 'USA';
+let continent = 'North America';
+let population = 334900000
+
+console.log("My country is the " + country + ", my continent is " + continent + ", and the population is " + population + ".");
+
+console.log(NAMEtyo); 
